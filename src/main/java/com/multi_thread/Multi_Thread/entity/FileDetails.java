804a7322f6189b49195afa078d669be0aaebdd19
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2025-03-14 16:28:18 +0530

    gnfgngdf

---
+++ src/main/java/com/multi_thread/Multi_Thread/entity/FileDetails.java
@@ -0,0 +1,37 @@
+package com.multi_thread.Multi_Thread.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FileDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileCode;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date uploadAt;
+
+    @Column(nullable = false)
+    private String checksum;
+
+    @Column(nullable = false)
+    private String status;
+
+
+}
